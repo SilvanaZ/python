f593b68e5cdf2f1da1dfcc30a68c4d23e0a35ad6
Silvana Zalazar <silvana.blaq@gmail.com>
2022-10-30 23:28:47 -0300

    First commit of python

---
+++ hola_mundo.py
@@ -0,0 +1,1 @@
+print('Estoy programando en Python!!\nTe amo mucho y me gusta imprimir en consola xD')
---
+++ variables.py
@@ -0,0 +1,25 @@
+"""
+Este es un comentario multilinea de Asignacion de variables, el cual Python no tendra cuenta a la hora de ejecutar el codigo
+"""
+
+num = 15680
+num2 = 2
+cadena = 'Damian, estoy "estudiando"'
+valor = True
+resultado = num + num2
+
+print(cadena)
+print(num2)
+print(valor)
+print('El resultado es:', resultado)
+print(type(num))
+print(type(cadena))
+print(type(valor))
+
+# Python permite el tipado Dinámico
+
+valor = 69
+print(valor)
+
+valor = "Silvana"
+print(valor)
