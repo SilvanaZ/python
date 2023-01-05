10f51db38409396c418ca14820236f419745eff6
Damian Fernandez <damianrocio12@gmail.com>
2023-01-04 22:15:28 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,28 @@
+# Operadores aritméticos
+num1 = 2
+num2 = 5
+
+resultadoOne = num1 + num2
+resultadoTwo = num1 ** num2
+
+print(resultadoOne)
+print(resultadoTwo)
+
+resultadoSuma = 10 + 5
+resultadoResta = 10 - 5
+resultadoMultiplicar = 10 * 5
+resultadoDividir = 10 / 5
+resultadoDividir2 = 10 // 5
+resultadoDividir3 = 10 % 5
+
+
+print(resultadoSuma)
+print(resultadoResta)
+print(resultadoMultiplicar)
+print(resultadoDividir)
+print(resultadoDividir2)
+print(resultadoDividir3)
+
+resultado = 3**3 * (13/5-(2*4))
+
+print(resultado)
